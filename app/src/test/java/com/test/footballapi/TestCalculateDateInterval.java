39f35eb60907df4afab6d6cdc9ad44cbd6401924
vladz <vladzuba96@gmail.com>
2021-01-27 01:06:05 +0300

    TODO add unit testes

--- app/src/test/java/com/test/footballapi/TestCalculateDateInterval.java
+++ app/src/test/java/com/test/footballapi/TestCalculateDateInterval.java
@@ -4,14 +4,16 @@ import com.test.footballapi.utils.CalculateDateInterval;
 
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestCalculateDateInterval {
     @Test
-    public void convertDateToString_isCorrect() {
-        Date date = new Date();
-        assertEquals(CalculateDateInterval.convertDateToString(date));
+    public void convertDateToString_isCorrect() throws ParseException {
+        Date date = new SimpleDateFormat( "yyyy-MM-dd" ).parse( "2020-12-15" );
+        assertEquals(CalculateDateInterval.convertDateToString(date), "2020-12-15");
     }
 }
